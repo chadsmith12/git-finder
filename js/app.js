b001ecb5962068ef10d259645e2fe3a2e24f544f
Chad Smith <chadsmith4@my.unt.edu>
2018-05-16 19:47:14 -0500

    added sort options/filters

--- js/app.js
+++ js/app.js
@@ -1,13 +1,21 @@
 const searchUser = document.getElementById('userSearch');
 const searchBtn = document.getElementById("searchBtn");
+const numberRepoSelect = document.getElementById("numberRepos");
+const sortBySelect = document.getElementById("sortBy");
+
 const gitHub = new GitHub();
 const ui = new GitHubUi();
 
 searchBtn.addEventListener('click', (e) => {
     const userText = searchUser.value;
+    const numberRepos = numberRepoSelect.value;
+    const sortBy = sortBySelect.value;
 
     if(userText !== '') {
-        var userData = gitHub.getUser(userText).then(data => {
+        var userData = gitHub.getUser(userText, {
+            reposCount: numberRepos,
+            repoSort: sortBy
+        }).then(data => {
             if(data.error) {
                 ui.showAlert('User could not be found', 'alert alert-danger')
             }
