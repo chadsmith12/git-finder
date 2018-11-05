7487e726812b065f61febb56e440bc2d8da800a0
Chad Smith <chadsmith4@my.unt.edu>
2018-11-05 00:50:55 -0600

    added GetUser request to the IGitHubApiService

--- server/GitFinderApiServer/GitHubApi/IGitHubApiService.cs
+++ server/GitFinderApiServer/GitHubApi/IGitHubApiService.cs
@@ -1,12 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
+using GitFinderApiServer.GitHubApi.Responses;
 
 namespace GitFinderApiServer.GitHubApi
 {
     public interface IGitHubApiService
     {
-
+        /// <summary>
+        /// Provides publicly available information about someone with a GitHub account.
+        /// </summary>
+        /// <param name="username">The username of the user you are searching for.</param>
+        /// <returns>The public information available.</returns>
+        Task<UserResponse> GetUser(string username);
     }
 }
